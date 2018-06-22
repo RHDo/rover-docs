ab247508c0616a29d59d31ca65ef65ed2a4ceaf3
Robert Hoettger <robert.hoettger@fh-dortmund.de>
2018-06-22 17:01:21 +0200

    minor fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+    # -*- coding: utf-8 -*-
 #
 # Rover documentation build configuration file, created by
 # sphinx-quickstart on Sun Oct 29 09:58:27 2017.
@@ -16,7 +16,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
