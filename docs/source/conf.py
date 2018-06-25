5e84b6d73e6f6381820c0fbfbb86dac8b0a31ecf
Robert Hoettger <robert.hoettger@fh-dortmund.de>
2018-06-25 09:14:29 +0200

    minor conf changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,9 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes"]
+numfig = True
 
 # -- General configuration ------------------------------------------------
 
@@ -97,8 +100,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = ["_themes",]
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
